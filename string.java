7dd651ead23f7d435cc215d535d09b6a7bafb0ff
Mallikarjun-29 <116905049+Mallikarjun-29@users.noreply.github.com>
2023-02-01 20:03:59 -0600

    Add files via upload

---
+++ string.java
@@ -0,0 +1,12 @@
+
+import java.io.*;
+import java.lang.*;
+
+class string{
+public static void main(String[] args){
+ String m ="Mallikharjun";
+System.out.println ("This is" + m);
+String m1 = new String("Mallikharjun");
+System.out.println("welcome" + m1);
+}
+}
