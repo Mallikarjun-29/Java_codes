453b28eeb6ca1cb2e6d1a93552d94da937a2f690
Mallikharjun <Mallelamallikarjun@gmail.com>
2023-02-07 15:25:46 -0600

    max with ternary operator

---
+++ maxter.java
@@ -0,0 +1,8 @@
+public class maxter {
+   public static void main(String[] args) {
+      int num1 = 10;
+      int num2 = 20;
+      int max = (num1 > num2) ? num1 : num2;
+      System.out.println("Max of two numbers is: " + max);
+   }
+}
